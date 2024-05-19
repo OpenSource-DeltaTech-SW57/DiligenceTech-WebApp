f6b37435f8a2aa7694a47f54c4b94c93d667234f
Abel Angel Ortega <abelangel2001@outlook.com>
2024-05-19 12:28:11 -0500

    fix: environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  serverBasePath: "https://jsondatabase-diligencetech.onrender.com"
+  //production: true,
+  //serverBasePath: "https://jsondatabase-diligencetech.onrender.com"
 };
