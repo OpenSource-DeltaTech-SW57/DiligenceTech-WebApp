b8f4f9760d034ecbb804d915b58200020dcd7b3f
Luis Eduardo Herrera Gonzalez <u202218227@upc.edu.pe>
2024-06-22 18:18:43 -0500

    feat(file-management): added structure for documents creation page.

--- src/app/file-management/components/documents-creation/documents-creation.component.ts
+++ src/app/file-management/components/documents-creation/documents-creation.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
+import {Project} from "../../../project-management/model/project.entity";
+import {NgForm} from "@angular/forms";
+import {Editor, Toolbar} from "ngx-editor";
+import {CustomizerSettingsService} from "../../../shared/services/customizer-settings.service";
+import {AreaApiService} from "../../services/area-api.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AreaRequest} from "../../model/area.request";
 
 @Component({
   selector: 'app-documents-creation',
@@ -6,5 +13,78 @@ import { Component } from '@angular/core';
   styleUrl: './documents-creation.component.scss'
 })
 export class DocumentsCreationComponent {
+  @Input() project: Project;
+  @Input() editMode = false;
+  @Output() projectAdded = new EventEmitter<Project>();
+  @Output() projectUpdated = new EventEmitter<Project>();
+  @Output() editCanceled = new EventEmitter();
+  @ViewChild('projectForm', {static:false}) projectForm!: NgForm;
+  areaName: string = '';
 
+  editor!: Editor;
+  toolbar: Toolbar = [
+    ['bold', 'italic'],
+    ['underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+  ]
+
+  ngOnInit(): void {
+    this.editor = new Editor();
+  }
+
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
+
+  public multiple: boolean = false;
+
+  isToggled = false;
+
+  constructor(
+    public themeService: CustomizerSettingsService,
+    private areaApiService: AreaApiService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.project = {} as Project;
+    this.themeService.isToggled$.subscribe(_isToggled => {
+      this.isToggled = _isToggled;
+    })
+  }
+
+  private resetEditState(){
+    this.editMode = false;
+    this.projectForm.resetForm();
+    this.project = {} as Project;
+  }
+
+  onSubmit() {
+    this.route.params.subscribe(params => {
+      let createArea = new AreaRequest(params['id'], this.areaName);
+      this.areaApiService.create(createArea).subscribe({
+        next: (response) => {
+          console.log(`Area Created: ${response.name}`);
+          this.router.navigate([`/${params['id']}/file-management`]);
+        },
+        error: (error) => {
+          console.error(`Error while creating area: ${error.message}`);
+        }
+      });
+    })
+  }
+
+  onCancel() {
+    this.resetEditState();
+    this.editCanceled.emit();
+  }
+
+  // RTL Mode
+  toggleRTLEnabledTheme() {
+    this.themeService.toggleRTLEnabledTheme();
+  }
 }
