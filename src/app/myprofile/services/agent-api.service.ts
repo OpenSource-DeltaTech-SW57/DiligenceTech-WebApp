38ce53f4cdc7a53b92d361f4511fcbb153cdc2bb
Abel Angel Ortega <abelangel2001@outlook.com>
2024-06-26 05:44:59 -0500

    feat: implemented agent-api-sevice

--- src/app/myprofile/services/agent-api.service.ts
+++ src/app/myprofile/services/agent-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../../../environments/environment";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, catchError, retry} from "rxjs";
 import {Router} from "@angular/router";
 import {BaseService} from "../../shared/services/base.service";
 import {Agent} from "../model/agent.entity";
@@ -17,11 +17,9 @@ export class AgentApiService extends BaseService<Agent>{
     this.resourceEndpoint = "/agents";
   }
 
-  getAgentByCode(agentCode: string){
-
-     agentCode = agentCode.replace('@','%40');
-     console.log(agentCode)
-     return  this.http.get<Agent>(`${this.basePath}${this.resourceEndpoint}/email/${agentCode}`).pipe();
+  getAgentByCode(agentCode: string) {
+    //agentCode = agentCode.replace('@', '%40');
+    return this.http.get<Agent>(`${this.basePath}${this.resourceEndpoint}/username/${agentCode}`).pipe();
   }
 
   getAgentDataByUsernameAndProjectId(agentRecordId: string, projectId: number) {
